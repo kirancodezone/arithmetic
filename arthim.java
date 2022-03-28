37633a820ee342e277ae4bd6080d04a1545d0233
kirancodezone <102456773+kirancodezone@users.noreply.github.com>
2022-03-28 20:46:53 +0530

    arithmetic
    All arithmetic operations

---
+++ arthim.java
@@ -0,0 +1,20 @@
+class Kiran{
+   
+   public static void main(String args[]){
+        int a=10,b=20,c;
+ 
+        c=a+b;
+        
+        System.out.println("add -- > "+c);
+        c=a-b;
+        System.out.println("sub -- > "+c);
+        c=a*b;
+        System.out.println("mul -- > "+c);
+        c=a/b;
+       System.out.println("div-- > "+c);
+
+   }
+
+
+
+}
